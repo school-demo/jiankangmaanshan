b93e3712ab6f01127255f8a16e8c035333fc0ccf
孙世伟 <sunshiwei@sunshiweideMacBook-Air.local>
2021-11-28 13:49:59 +0800

    ss

---
+++ stu/src/com/stu/ss/Hello.java
@@ -0,0 +1,11 @@
+package com.stu.ss;
+
+/**
+ * @author ssw
+ * @date 2021年11月28日 1:49 下午
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("ssw");
+    }
+}

67ceff698a0d125773967ec67e37e5c57006ae67
zc <2497534758@qq.com>
2021-11-28 13:41:07 +0800

    helloword

---
+++ stu/src/com/stu/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.stu;
+
+/**
+ * @version 1.0
+ * @Author ZC
+ * @date 2021/11/28 13:40
+ * @desc
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
